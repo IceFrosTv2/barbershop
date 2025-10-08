7a24df60aa51441538c49ff16a43d50da86653cf
Andrei Sergheev <chaos95kill@gmail.com>
2025-10-08 20:56:49 +0300

    Added footer

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,10 @@
 $(function () {
     $('.parallax').parallax({imageSrc: '../image/bg-follow-full.png', speed: .8});
     $('.parallax2').parallax({imageSrc: '../image/bg-follow-full.png', speed: .5});
+    const thumbnails = document.getElementsByClassName('thumbnail');
+    let current;
+    ymaps.ready(init);
+    let center = [55.9174276443194,37.99588015321116];
 
     const splide = new Splide('#main-slide', {
         pagination: false,
@@ -28,9 +32,6 @@ $(function () {
     splide.mount();
     thumbnailSlider.mount();
 
-    const thumbnails = document.getElementsByClassName('thumbnail');
-    let current;
-
     for (let i = 0; i < thumbnails.length; i++) {
         barberList(thumbnails[i], i);
     }
@@ -54,4 +55,48 @@ $(function () {
         }
     });
 
-})
+    function init() {
+        var map = new ymaps.Map("footer-map", {
+            center: center,
+            zoom: 15,
+        });
+
+        let placemark = new ymaps.Placemark(center, {}, {
+            iconLayout: 'default#image',
+            iconImageHref: '../image/icon-geo.svg',
+            iconImageSize: [40, 50],
+            iconImageOffset: [-20, -40]
+        });
+
+        map.controls.remove('geolocationControl'); // удаляем геолокацию
+        map.controls.remove('searchControl'); // удаляем поиск
+        map.controls.remove('trafficControl'); // удаляем контроль трафика
+        map.controls.remove('typeSelector'); // удаляем тип
+        map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+        map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+        map.controls.remove('rulerControl'); // удаляем контрол правил
+        // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+        map.geoObjects.add(placemark); // помещаем флажок на карту
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
